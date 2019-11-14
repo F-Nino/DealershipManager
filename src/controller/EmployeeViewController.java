badc05bdbfdfb0316c2d6cdb5a99ba91fd064836
nengler <englernathan@gmail.com>
2019-11-13 20:12:25 -0600

    continued work on employee tableview

--- src/controller/EmployeeViewController.java
+++ src/controller/EmployeeViewController.java
@@ -24,12 +24,10 @@ import model.EmployeeDAO;
 public class EmployeeViewController implements Initializable {
 
 	private EmployeeDAO empDAO;
-	private int carYearText;
-	private int carPriceText;
 	private ArrayList<EmployeeObject> employeeObjectArrayList = new ArrayList<EmployeeObject>();
 
 	@FXML
-	private ComboBox comboBoxCars;
+	private ComboBox comboBoxEmps;
 
 	@FXML
 	private TextField carBrand;
@@ -50,35 +48,26 @@ public class EmployeeViewController implements Initializable {
 	private Text informationTxt;
 
 	@FXML
-	private TableColumn<CarObject, Integer> carIDColumn;
+	private TableColumn<EmployeeObject, Integer> empIDColumn;
 
 	@FXML
-	private TableColumn<CarObject, String> carBrandColumn;
+	private TableColumn<EmployeeObject, String> empFirstNameColumn;
 
 	@FXML
-	private TableColumn<CarObject, String> carNameColumn;
+	private TableColumn<EmployeeObject, String> empLastNameColumn;
 
 	@FXML
-	private TableColumn<CarObject, String> carColorColumn;
-
-	@FXML
-	private TableColumn<CarObject, Integer> carYearColumn;
-
-	@FXML
-	private TableColumn<CarObject, Integer> carPriceColumn;
-
-	@FXML
-	private TableView<CarObject> carTableView;
+	private TableView<EmployeeObject> employeeTableView;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// Calling here to check if table exists.
-		carDAO = new CarDAO();
-		comboBoxCars.getItems().clear();
-		comboBoxCars.getItems().addAll("Car Brand", "Car Name", "Car Color", "Car Year", "Car Price");
-		comboBoxCars.getSelectionModel().select("Car Brand");
+		empDAO = new EmployeeDAO();
+		comboBoxEmps.getItems().clear();
+		comboBoxEmps.getItems().addAll("Employee ID", "First Name", "Last Name");
+		comboBoxEmps.getSelectionModel().select("Employee ID");
 		try {
-			loadCarDetails();
+			loadEmployeeDetails();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -87,20 +76,18 @@ public class EmployeeViewController implements Initializable {
 	}
 
 	@FXML
-	public void loadCarDetails() throws SQLException {
-		carIDColumn.setCellValueFactory(new PropertyValueFactory<CarObject, Integer>("carID"));
-		carBrandColumn.setCellValueFactory(new PropertyValueFactory<CarObject, String>("carBrand"));
-		carNameColumn.setCellValueFactory(new PropertyValueFactory<CarObject, String>("carName"));
-		carColorColumn.setCellValueFactory(new PropertyValueFactory<CarObject, String>("carColor"));
-		carYearColumn.setCellValueFactory(new PropertyValueFactory<CarObject, Integer>("carYear"));
-		carPriceColumn.setCellValueFactory(new PropertyValueFactory<CarObject, Integer>("carPrice"));
-		ResultSet carsQuery = carDAO.returnCars();
-		if (carsQuery == null) {
+	public void loadEmployeeDetails() throws SQLException {
+		empIDColumn.setCellValueFactory(new PropertyValueFactory<EmployeeObject, Integer>("employeeID"));
+		empFirstNameColumn.setCellValueFactory(new PropertyValueFactory<EmployeeObject, String>("firstName"));
+		empLastNameColumn.setCellValueFactory(new PropertyValueFactory<EmployeeObject, String>("lastName"));
+		ResultSet employeeQuery = empDAO.returnCustomers();
+		if (empDAO == null) {
 			System.out.println("that sucks");
 		} else {
-			while (carsQuery.next()) {
-				carObjectArrayList.add(new CarObject(carsQuery.getInt(1), carsQuery.getString(2),
-						carsQuery.getString(3), carsQuery.getString(4), carsQuery.getInt(5), carsQuery.getInt(6)));
+			while (employeeQuery.next()) {
+				employeeObjectArrayList.add(new EmployeeObject(employeeQuery.getInt("employeeid"),
+						employeeQuery.getString("employeeid"), employeeQuery.getString("firstName"),
+						employeeQuery.getString("lastName"), employeeQuery.getInt("adminPriviliges")));
 			}
 			changeSort();
 		}
@@ -108,63 +95,41 @@ public class EmployeeViewController implements Initializable {
 	}
 
 	@FXML
-	public void sortCars() {
-
-	}
-
-	@FXML
 	public void changeSort() {
-		// "Car Name", "Car Color", "Car Year", "Car Price"
-		String comboBoxValue = (String) comboBoxCars.getValue();
+		String comboBoxValue = (String) comboBoxEmps.getValue();
 		// bubble sort
-		if (comboBoxValue.equals("Car Brand")) {
-			for (int i = 0; i < carObjectArrayList.size() - 1; i++) {
-				for (int j = 0; j < carObjectArrayList.size() - i - 1; j++) {
-					if ((carObjectArrayList.get(j).getCarBrand()
-							.compareTo(carObjectArrayList.get(j + 1).getCarBrand())) > 0) {
-						Collections.swap(carObjectArrayList, j, j + 1);
+		if (comboBoxEmps.equals("Employee ID")) {
+			for (int i = 0; i < employeeObjectArrayList.size() - 1; i++) {
+				for (int j = 0; j < employeeObjectArrayList.size() - i - 1; j++) {
+					if (employeeObjectArrayList.get(j).getEmployeeID() > employeeObjectArrayList.get(j + 1)
+							.getEmployeeID()) {
+						Collections.swap(employeeObjectArrayList, j, j + 1);
 					}
 				}
 			}
-		} else if (comboBoxValue.equals("Car Name")) {
-			for (int i = 0; i < carObjectArrayList.size() - 1; i++) {
-				for (int j = 0; j < carObjectArrayList.size() - i - 1; j++) {
-					if ((carObjectArrayList.get(j).getCarName()
-							.compareTo(carObjectArrayList.get(j + 1).getCarName())) > 0) {
-						Collections.swap(carObjectArrayList, j, j + 1);
+		} else if (comboBoxEmps.equals("First Name")) {
+			for (int i = 0; i < employeeObjectArrayList.size() - 1; i++) {
+				for (int j = 0; j < employeeObjectArrayList.size() - i - 1; j++) {
+					if ((employeeObjectArrayList.get(j).getFirstName()
+							.compareTo(employeeObjectArrayList.get(j + 1).getFirstName())) > 0) {
+						Collections.swap(employeeObjectArrayList, j, j + 1);
 					}
 				}
 			}
-		} else if (comboBoxValue.equals("Car Color")) {
-			for (int i = 0; i < carObjectArrayList.size() - 1; i++) {
-				for (int j = 0; j < carObjectArrayList.size() - i - 1; j++) {
-					if ((carObjectArrayList.get(j).getCarColor()
-							.compareTo(carObjectArrayList.get(j + 1).getCarColor())) > 0) {
-						Collections.swap(carObjectArrayList, j, j + 1);
-					}
-				}
-			}
-		} else if (comboBoxValue.equals("Car Year")) {
-			for (int i = 0; i < carObjectArrayList.size() - 1; i++) {
-				for (int j = 0; j < carObjectArrayList.size() - i - 1; j++) {
-					if (carObjectArrayList.get(j).getCarYear() > (carObjectArrayList.get(j + 1).getCarYear())) {
-						Collections.swap(carObjectArrayList, j, j + 1);
-					}
-				}
-			}
-		} else if (comboBoxValue.equals("Car Price")) {
-			for (int i = 0; i < carObjectArrayList.size() - 1; i++) {
-				for (int j = 0; j < carObjectArrayList.size() - i - 1; j++) {
-					if (carObjectArrayList.get(j).getCarPrice() > (carObjectArrayList.get(j + 1).getCarPrice())) {
-						Collections.swap(carObjectArrayList, j, j + 1);
+		} else if (comboBoxEmps.equals("Last Name")) {
+			for (int i = 0; i < employeeObjectArrayList.size() - 1; i++) {
+				for (int j = 0; j < employeeObjectArrayList.size() - i - 1; j++) {
+					if ((employeeObjectArrayList.get(j).getLastName()
+							.compareTo(employeeObjectArrayList.get(j + 1).getLastName())) > 0) {
+						Collections.swap(employeeObjectArrayList, j, j + 1);
 					}
 				}
 			}
 		}
 
-		carTableView.getItems().clear();
-		for (CarObject coo : carObjectArrayList) {
-			carTableView.getItems().add(coo);
+		employeeTableView.getItems().clear();
+		for (EmployeeObject coo : employeeObjectArrayList) {
+			employeeTableView.getItems().add(coo);
 		}
 
 	}
