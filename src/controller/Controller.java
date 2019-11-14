77fb9d8c62b62b82de805c8a1c8f19055edd936b
Felipe <ninofelipe73@gmail.com>
2019-11-13 19:46:31 -0600

    changed id's for tableview and columns

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -17,78 +17,79 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import model.UserDAO;
 
 public class Controller implements Initializable {
-	
+
 	private UserDAO userDAO;
 	private ArrayList<UserObject> userObjectArrayList = new ArrayList<UserObject>();
-	
-	 public void initialize(URL location, ResourceBundle resources) {
-	        //Calling here to check if table exists.
-	       
-	       userDAO = new UserDAO();
-	    }
-	 
-	    @FXML
-	    private TextField fName;
-	    
-	    @FXML
-	    private TextField lName;
-	    
-	    @FXML
-	    private TextField username;
-	    @FXML 
-	    private TextField password;
 
-	    @FXML
-	    private TextField email;
-	    @FXML
-	    private TextField age;
-	    @FXML
-	    private TextField DOB;
-	    @FXML
-	    private TableColumn <UserObject, String> userfName;
-	    @FXML
-	    private TableColumn <UserObject, String> userlName;
-	    @FXML
-	    private TableColumn <UserObject, String> userUsername;
-	    @FXML
-	    private TableColumn <UserObject, String> userPassword;
-	    @FXML
-	    private TableColumn <UserObject, String> userEmail;
-	    @FXML
-	    private TableColumn <UserObject, String> userAge;
-	    @FXML
-	    private TableColumn <UserObject, String> userDOB;
-	    
-	    @FXML 
-	    private TableView<UserObject> userTableView;
-	    
-	 
-	 public void onClickAddUser() {
-		 userDAO.addNewUser(fName.getText(), lName.getText(), username.getText(), password.getText(), email.getText(), age.getText(), DOB.getText());
-	 }
-	 
+	public void initialize(URL location, ResourceBundle resources) {
+		// Calling here to check if table exists.
+
+		userDAO = new UserDAO();
+	}
+
+	@FXML
+	private TextField fName;
+
+	@FXML
+	private TextField lName;
+
+	@FXML
+	private TextField username;
+	@FXML
+	private TextField password;
+
+	@FXML
+	private TextField email;
+	@FXML
+	private TextField age;
+	@FXML
+	private TextField DOB;
+	@FXML
+	private TableColumn<UserObject, String> userfName;
+	@FXML
+	private TableColumn<UserObject, String> userlName;
+	@FXML
+	private TableColumn<UserObject, String> userUsername;
+	@FXML
+	private TableColumn<UserObject, String> userPassword;
+	@FXML
+	private TableColumn<UserObject, String> userEmail;
+	@FXML
+	private TableColumn<UserObject, String> userAge;
+	@FXML
+	private TableColumn<UserObject, String> userDOB;
+
+	@FXML
+	private TableView<UserObject> userTableView;
+
+	public void onClickAddUser() {
+		userDAO.addNewUser(fName.getText(), lName.getText(), username.getText(), password.getText(), email.getText(),
+				age.getText(), DOB.getText());
+	}
+
 	public void grabUsers() {
 		userDAO.grabUsers();
 	}
-	
+
 	@FXML
 	public void loadUserDetails() throws SQLException {
-	userfName.setCellValueFactory(new PropertyValueFactory<UserObject, String>("fname"));
-	userlName.setCellValueFactory(new PropertyValueFactory<UserObject, String>("lname"));
-	userUsername.setCellValueFactory(new PropertyValueFactory<UserObject, String>("username"));
-	userPassword.setCellValueFactory(new PropertyValueFactory<UserObject, String>("password"));
-	userEmail.setCellValueFactory(new PropertyValueFactory<UserObject, String>("email"));
-	userAge.setCellValueFactory(new PropertyValueFactory<UserObject, String>("age"));
-	userDOB.setCellValueFactory(new PropertyValueFactory<UserObject, String>("DOB"));
-	ResultSet usersQuery = UserDAO.grabUsers();
-	if (usersQuery == null) {
-	System.out.println("that sucks lol nate");
-	} else {
-	while (usersQuery.next()) {
-	userObjectArrayList.add(new UserObject(usersQuery.getString(1), usersQuery.getString(2),
-	usersQuery.getString(3), usersQuery.getString(4), usersQuery.getString(5),usersQuery.getString(6), usersQuery.getString(7)));
-	}
-	
-	}
+		userfName.setCellValueFactory(new PropertyValueFactory<UserObject, String>("fname"));
+		userlName.setCellValueFactory(new PropertyValueFactory<UserObject, String>("lname"));
+		userUsername.setCellValueFactory(new PropertyValueFactory<UserObject, String>("username"));
+		userPassword.setCellValueFactory(new PropertyValueFactory<UserObject, String>("password"));
+		userEmail.setCellValueFactory(new PropertyValueFactory<UserObject, String>("email"));
+		userAge.setCellValueFactory(new PropertyValueFactory<UserObject, String>("age"));
+		userDOB.setCellValueFactory(new PropertyValueFactory<UserObject, String>("DOB"));
+		ResultSet usersQuery = userDAO.grabUsers();
+		if (usersQuery == null) {
+			System.out.println("that sucks lol nate");
+		} else {
+			while (usersQuery.next()) {
+				userObjectArrayList.add(new UserObject(usersQuery.getString(1), usersQuery.getString(2),
+						usersQuery.getString(3), usersQuery.getString(4), usersQuery.getString(5),
+						usersQuery.getString(6), usersQuery.getString(7)));
+			}
+
+		}
 	}
 }
